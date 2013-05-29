76e07768a1fb0d360e780bac54eb8b2b3e58d5c7
Rasmus Kristensen <github@rasmuskr.dk>
2013-05-29 16:54:51 +0000

    - Fixing so single value returns from queries like: DECLARE @test int SELECT @test = 5 SELECT @test as test works correctly - also fixed so as_dict works correctly by getting the right values out

--- pytds/tds.py
+++ pytds/tds.py
@@ -3369,7 +3369,10 @@ class _TdsSession(object):
         cols = self.res_info.columns
         row = tuple(self.row)
         if as_dict:
-            row = dict((col.column_name, col.value) for col in cols if col.column_name)
+            dictRow = {}
+            for i, col in enumerate(cols):
+                dictRow[col.column_name] = row[i]
+            return dictRow
         return row
 
     def next_row(self):
@@ -3395,8 +3398,8 @@ class _TdsSession(object):
                 return True
             elif marker in (TDS_DONE_TOKEN, TDS_DONEPROC_TOKEN, TDS_DONEINPROC_TOKEN):
                 self.process_end(marker)
-                if self.done_flags & TDS_DONE_MORE_RESULTS and not self.done_flags & TDS_DONE_COUNT:
-                    # skip results that don't event have rowcount
+                if self.done_flags & TDS_DONE_MORE_RESULTS:
+                    # even take result that have no rowcount as it might be a single value
                     continue
                 return False
             else:
@@ -3412,8 +3415,8 @@ class _TdsSession(object):
                 return True
             elif marker in (TDS_DONE_TOKEN, TDS_DONEPROC_TOKEN):
                 self.process_end(marker)
-                if self.done_flags & TDS_DONE_MORE_RESULTS and not self.done_flags & TDS_DONE_COUNT:
-                    # skip results that don't event have rowcount
+                if self.done_flags & TDS_DONE_MORE_RESULTS:
+                    # even take result that have no rowcount as it might be a single value
                     continue
                 return False
             else:
